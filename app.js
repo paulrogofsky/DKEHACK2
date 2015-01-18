b5fd4cbff55d3d079add873883ed60eb892f498d
paulrogofsky <paul.rogofsky@quovo.com>
2015-01-17 21:24:39 -0500

    app

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('cookie-session');
+
 var app = express();
+
+var debug = require('debug')('SpotList');
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
 });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'stylesheets')));
+app.use(express.static(path.join(__dirname, 'frontEndScripts')));
+// app.set('view engine', 'hjs');
 app.engine('html',require('hogan-express'));
 app.set('view engine', 'html')
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'PIZZA'}));
-console.log("HELLO WORLD");
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
+console.log("Hello world!");
