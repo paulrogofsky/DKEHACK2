e92ffa336238401ca36bbccf2c1735149660bbed
paulrogofsky <paul.rogofsky@quovo.com>
2015-01-17 20:29:35 -0500

    Node setup

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('cookie-session');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+app.engine('html',require('hogan-express'));
+app.set('view engine', 'html')
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({secret: 'PIZZA'}));
 console.log("HELLO WORLD");
