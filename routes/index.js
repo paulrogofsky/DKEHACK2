adf01a9460259f7042b3b95233bb3ead7d23a6d1
paulrogofsky <paul.rogofsky@quovo.com>
2015-01-17 22:22:56 -0500

    setup for actual coding!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var request = require ("request");
 
 /* GET home page. */
 router.get('/', function(req, res) {
 	res.render('index');
 });
+
+router.post('/subpage', function(req, res, next) {
+  if (req.body.url === '' || req.body.url === undefined) {
+    req.session.alert = 'You must provide a url in order to add to your playlist';
+    res.render('index');//failure
+  } else {
+  	scraper(req.body.url);
+  }
+});
+
+//void methods below
+
+//should call addToSpotify within this method
+function scraper (url) {
+
+}
+
+//listInfo of form [(song,artist)]
+function addToSpotify (listInfo) {
+
+}
 
 module.exports = router;
