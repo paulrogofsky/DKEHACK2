ce6ef4035dc411257e3ef7d6515933431ecffe36
paulrogofsky <paul.rogofsky@quovo.com>
2015-01-17 23:49:34 -0500

    scraper now ready to go

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,54 @@ var request = require ("request");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index');
+	res.render('index',{additional:"Good"});
 });
 
 router.post('/subpage', function(req, res, next) {
-  if (req.body.url === '' || req.body.url === undefined) {
-    req.session.alert = 'You must provide a url in order to add to your playlist';
-    res.render('index');//failure
-  } else {
-  	scraper(req.body.url);
-  }
+ 	if (req.body.url === '' || req.body.url === undefined) {
+		req.session.alert = 'You must provide a url in order to add to your playlist';
+		res.render('index',{additional:'You typed in an invalid url1!'});//failure
+	} else {
+  		getPage(res,req.body.url);
+  	}
 });
 
 //void methods below
 
-//should call addToSpotify within this method
-function scraper (url) {
+function getPage (res,url) {
+	if (url === '' || url === undefined) {
+		res.render('index',{additional:'You typed in an invalid url2!'});//failure
+	}
+	else {
+		request(
+			{
+				uri: url
+				, method: "GET" 
+				,  timeout: 10000
+	  			, followRedirect: true
+	  			, maxRedirects: 10
+			}
+			, function(error, response, body) {
+				if (error === null) {
+					console.log(error);
+					scraper(res,body);
+					res.redirect("/");//MAKE SOMETHING HERE!!!
+				}
+				else {
+					res.render('index',{additional:'You typed in an invalid url3!'});
+				}
+			}
+		);
+	}
+}
+
+function scraper (res,document) {
 
 }
 
-//listInfo of form [(song,artist)]
+//listInfo of form, 
 function addToSpotify (listInfo) {
-
+	
 }
 
 module.exports = router;
